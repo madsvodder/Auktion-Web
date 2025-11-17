c5394a39d08854ed6a24351177b6925ddb4b8c2e
madsvodder <madsvodder@gmail.com>
2025-11-17 21:32:11 +0100

    Sidebar
    - Changed auction card to bootstrap
    - Sidebar

--- src/app/pages/auctions-page/auctions-page.ts
+++ src/app/pages/auctions-page/auctions-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {AuctionCard} from '../../components/auction-card/auction-card';
+import {Sidebar} from '../../components/sidebar/sidebar';
 
 @Component({
   selector: 'app-auctions-page',
   imports: [
-    AuctionCard
+    AuctionCard,
+    Sidebar
   ],
   templateUrl: './auctions-page.html',
   styleUrl: './auctions-page.css',
